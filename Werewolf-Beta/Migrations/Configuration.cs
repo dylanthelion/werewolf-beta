61bcd8d3931fb14fb5bf181fbe7fe008af25535b
dylanthelion <dillion256@gmail.com>
2016-09-25 21:10:41 -0700

    Re0placed User Seed with bulk insert method

--- Werewolf-Beta/Migrations/Configuration.cs
+++ Werewolf-Beta/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace Werewolf_Beta.Migrations
 {
+    using EntityFramework.BulkInsert.Extensions;
     using Models;
     using System;
+    using System.Data;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,7 +17,7 @@ namespace Werewolf_Beta.Migrations
 
         protected override void Seed(Werewolf_Beta.Models.WerewolfContext context)
         {
-            if(context.AllUsers.ToList().Count <= 0)
+            /*if(context.AllUsers.ToList().Count <= 0)
             {
                 for (int x = 0; x < 100; x++)
                 {
@@ -26,6 +28,19 @@ namespace Werewolf_Beta.Migrations
                     context.AllUsers.Add(u);
                 }
                 context.SaveChanges();
+            }*/
+
+            if (!context.AllUsers.Any())
+            {
+                var items = Enumerable.Range(0, 100000)
+                          .Select(s => new User
+                          {
+                              ID = s,
+                              UserName = String.Format("TestName{0}", s),
+                              Password = String.Format("TestPassword{0}", s)
+                          });
+
+                context.BulkInsert(items, 1000);
             }
         }
     }
